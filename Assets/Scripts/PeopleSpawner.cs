307a88fef6340e1e3d0de2fefb2b607a20bb3ed7
HerrBrunke <34299221+HerrBrunke@users.noreply.github.com>
2018-11-23 13:22:18 +0100

    Init
    letzego

---
+++ Assets/Scripts/PeopleSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleSpawner : MonoBehaviour {
+	public int length = 10;
+	public Sprite[] spriteContainer;
+	public int rndNumber;
+	public bool isSpawned = false;
+
+	private void Awake()
+	{
+		isSpawned = false;
+		rndNumber = Random.Range(1, 5);
+	}
+	// Use this for initialization
+	void Start () {
+		
+		GenerateMessage(spriteContainer[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GenerateMessage(Sprite sprite) {
+		for (int z = 0; z < length; z++)
+		{
+			for (int x = 0; x < length; x++)
+			{
+				Debug.Log(rndNumber);
+				//
+				if (!isSpawned && z % (x+1) == rndNumber) {
+					Debug.Log("Alarm!");
+					Debug.Log(rndNumber);
+
+				}
+				//
+				GameObject obj = new GameObject("boy");
+				SpriteRenderer renderer = obj.AddComponent<SpriteRenderer>();
+				renderer.sprite = sprite;
+				int posX = (x + 1) * x -10;
+				int posZ = (z + 1) * x + 10;
+				obj.transform.position = new Vector3(posX, posZ, -5);
+				Debug.LogFormat("x: " + x + " z: " + z);
+			}
+		}
+	}
+	////start
+	//global bool isSpawned = false;
+	//int rndNumber = Random.Range(0, 10);
+	////inMapGenerator
+	//void SpwanRandom(Vector2Int dimension) {
+
+	//	if (!isSpawned && y % x == rndNumber) {
+	//		isSpawned = true;
+	//		Instantiate<Player>(Player(dimension));
+	//	}
+	//}
+}
---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour {
+	public int mapLength = 10;
+	public GameObject Terrain;
+	public List<GameObject> Tiles;
+	private void Start()
+	{
+		GenerateMap();
+	}
+
+	void GenerateMap()
+	{
+		for (int x = 0; x < mapLength; x++)
+		{
+			for (int y = 0; y < mapLength; y++)
+			{
+				Instantiate(Tiles[Random.Range(0, 4)], new Vector3(x, y, 0), Quaternion.identity, Terrain.transform);
+			}
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButton(0)) {
+			GenerateMap();
+		}
+	}
+
+}
